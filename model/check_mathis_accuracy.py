b5322bd4da76dd206a6e607ca48ef5304642c216
Christopher Canel <c.canel@icloud.com>
2020-07-09 01:02:26 +0000

    Discard packets with Mathis Model label == -1

--- model/check_mathis_accuracy.py
+++ model/check_mathis_accuracy.py
@@ -28,22 +28,29 @@ def main():
 
     correct = 0
     total = 0
+    total_all = 0
     for flp in os.listdir(exp_dir):
         dat = np.load(path.join(exp_dir, flp))
         dat = dat[dat.files[0]]
         sim = utils.Sim(flp)
+        labels = dat["mathis model label"]
+        valid = np.where(labels != -1)
         correct += (
-            (dat["mathis model label"] ==
+            (labels[valid] ==
              # To determine the ground truth, compare the actual queue
              # occupancy to the fair queue occupancy (convert to ints to
              # generate class labels).
-             (dat["queue occupancy ewma-alpha0.5"] > (
+             (dat["queue occupancy ewma-alpha0.5"][valid] > (
                  1 / (sim.unfair_flws + sim.other_flws))).astype(int)
              # Count the number of correct predictions by converting
              # from bools to ints and summing them up.
             ).astype(int).sum())
-        total += dat.shape[0]
+        total += len(valid[0])
+        total_all += dat.shape[0]
     print(f"Mathis Model accuracy: {(correct / total) * 100:.2f}%")
+    print(f"Total: {total} packets")
+    print(f"Discarded {total_all - total} packets.")
+    print(f"Accuracy without discarding packets: {(correct / total_all) * 100:.2f}%")
 
 
 if __name__ == "__main__":
