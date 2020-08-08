8f19c90e27903e54d8170d6df9d5814555bb2c31
Zecheng He <zechengh@andrew.cmu.edu>
2020-08-08 16:24:47 -0400

    Update test_all.py to read from temp file

--- model/test_all.py
+++ model/test_all.py
@@ -17,45 +17,51 @@ import models
 import train
 import utils
 
+manager = multiprocessing.Manager()
+all_accuracy = manager.list()
 
-all_accuracy = []
-
-# Bandwidth in Mbps
-bw_dict = {
-    1: [],
-    10: [],
-    30: [],
-    50: [],
-    1000: []
-}
+bw_dict = manager.dict({
+    1: manager.list(),
+    10: manager.list(),
+    30: manager.list(),
+    50: manager.list(),
+    1000: manager.list()
+})
 
 # RTT in us
-rtt_dict = {
-    1000: [],
-    10000: [],
-    50000: [],
-    100000: [],
-    1000000: []
-}
+rtt_dict = manager.dict({
+    1000: manager.list(),
+    10000: manager.list(),
+    50000: manager.list(),
+    100000: manager.list(),
+    1000000: manager.list()
+})
 
-# Queue size in number of packets
-queue_dict = {
-    10: [],
-    50: [],
-    100: [],
-    200: [],
-    500: [],
-    1000: [],
-    5000: []
-}
 
-def process_one(sim_flp, out_dir, net, warmup_prc, scl_prms_flp, standardize):
+queue_dict = manager.dict({
+    10: manager.list(),
+    50: manager.list(),
+    100: manager.list(),
+    200: manager.list(),
+    500: manager.list(),
+    1000: manager.list(),
+    5000: manager.list()
+})
+
+def process_one(sim_flp, out_dir, net, warmup_prc, scl_prms_flp, standardize, all_accuracy, 
+                bw_dict, rtt_dict, queue_dict):
+
     """ Evaluate a single simulation. """
+    if not path.exists(out_dir):
+        os.makedirs(out_dir)
+
     # Load and parse the simulation.
-    (dat_in, dat_out, dat_out_raw, dat_out_oracle, _), sim = (
+    temp_path, sim = (
         train.process_sim(
             idx=0, total=1, net=net, sim_flp=sim_flp, tmp_dir=out_dir,
             warmup_prc=warmup_prc, keep_prc=100, sequential=True))
+
+    (dat_in, dat_out, dat_out_raw, dat_out_oracle, _) = utils.load_tmp_file(temp_path)    
 
     # Load and apply the scaling parameters.
     with open(scl_prms_flp, "r") as fil:
@@ -64,9 +70,6 @@ def process_one(sim_flp, out_dir, net, warmup_prc, scl_prms_flp, standardize):
 
     # Visualize the ground truth data.
     utils.visualize_classes(net, dat_out)
-
-    if not path.exists(out_dir):
-        os.makedirs(out_dir)
 
     # Test the simulation.
     accuracy = net.test(
@@ -88,40 +91,40 @@ def process_one(sim_flp, out_dir, net, warmup_prc, scl_prms_flp, standardize):
     all_accuracy.append(accuracy)
     mean_accuracy = mean(all_accuracy)
 
-    for bw_Mbps in bw_dict:
+    for bw_Mbps in bw_dict.keys():
         if sim.bw_Mbps <= bw_Mbps:
             bw_dict[bw_Mbps].append(accuracy)
             break
 
     rtt_us = (sim.btl_delay_us + 2 * sim.edge_delays[0]) * 2
-    for rtt_us_ in rtt_dict:
+    for rtt_us_ in rtt_dict.keys():
         if rtt_us <= rtt_us_:
             rtt_dict[rtt_us_].append(accuracy)
             break
 
-    for queue_p in queue_dict:
+    for queue_p in queue_dict.keys():
         if sim.queue_p <= queue_p:
             queue_dict[queue_p].append(accuracy)
             break
 
     print(
         f"Finish processing {sim.name}\n"
-        f"Average accuracy for all the processed simulations: {mean_accuracy}")
+        f"----Average accuracy for all the processed simulations: {mean_accuracy}")
 
-    for bw_Mbps in bw_dict:
+    for bw_Mbps in bw_dict.keys():
         if bw_dict[bw_Mbps]:
-            mean_accuracy = mean(bw_dict[bw_Mbps])
-            print(f"Bandwidth less than {bw_Mbps}Mbps accuracy {mean_accuracy}")
+            bw_accuracy = mean(bw_dict[bw_Mbps])
+            print(f"----Bandwidth less than {bw_Mbps}Mbps accuracy {bw_accuracy}")
 
-    for rtt_us_ in rtt_dict:
+    for rtt_us_ in rtt_dict.keys():
         if rtt_dict[rtt_us_]:
-            mean_accuracy = mean(rtt_dict[rtt_us_])
-            print(f"Rtt less than {rtt_us_}ns accuracy {mean_accuracy}")
+            rtt_accuracy = mean(rtt_dict[rtt_us_])
+            print(f"----Rtt less than {rtt_us_}ns accuracy {rtt_accuracy}")
 
-    for queue_p in queue_dict:
+    for queue_p in queue_dict.keys():
         if queue_dict[queue_p]:
-            mean_accuracy = mean(queue_dict[queue_p])
-            print(f"Queue size less than {queue_p} packets accuracy {mean_accuracy}")
+            queue_accuracy = mean(queue_dict[queue_p])
+            print(f"----Queue size less than {queue_p} packets accuracy {queue_accuracy}")
 
 
 def main():
@@ -190,7 +193,8 @@ def main():
 
     func_input = [
         (path.join(sim_dir, sim), path.join(out_dir, sim.split(".")[0]), net,
-         warmup_prc, scl_prms_flp, standardize)
+         warmup_prc, scl_prms_flp, standardize, all_accuracy, bw_dict,
+         rtt_dict, queue_dict)
         for sim in sorted(os.listdir(sim_dir))]
 
     print(f"Num files: {len(func_input)}")
