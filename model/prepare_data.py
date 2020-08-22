60fc9bc3bd3d349b8ac3c192432e56ce648df349
Christopher Canel <c.canel@icloud.com>
2020-08-22 17:40:07 +0000

    Add skeleton file

---
+++ model/prepare_data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Reads parsed simulation files created by parse_dumbell.py and
+creates unified training, validation, and test sets.
+"""
+
+import argparse
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
