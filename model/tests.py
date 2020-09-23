d6dc738a7491494490c5680530d7a95ddde78848
Christopher Canel <c.canel@icloud.com>
2020-09-23 20:00:44 +0000

    Make separate test for dependencies

--- model/tests.py
+++ model/tests.py
@@ -5,23 +5,6 @@ sys.path.append(path.dirname(path.realpath(__file__)))
 
 import unittest
 
-import check_mathis_accuracy
-import cl_args
-import correlation
-import defaults
-import fet_hists
-import gen_training_data
-import graph_one
-import hyper
-import models
-import parse_dumbbell
-import prepare_data
-import sim
-import test
-import train
-import training_param_sweep
-import utils
-
 
 class TestThings(unittest.TestCase):
 
@@ -33,6 +16,24 @@ class TestThings(unittest.TestCase):
 
     def test_three(self):
         self.assertEqual(3 + 3, 5)
+
+    def test_deps(self):
+        import check_mathis_accuracy
+        import cl_args
+        import correlation
+        import defaults
+        import fet_hists
+        import gen_training_data
+        import graph_one
+        import hyper
+        import models
+        import parse_dumbbell
+        import prepare_data
+        import sim
+        import test
+        import train
+        import training_param_sweep
+        import utils
 
 
 if __name__ == "__main__":
