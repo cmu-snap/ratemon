040eb686c3b43cdb8ceff6e0331b88f074ac77dd
Christopher Canel <c.canel@icloud.com>
2020-10-15 01:57:12 +0000

    Implement figure 1c from Ray's paper

--- model/validate_sim.py
+++ model/validate_sim.py
@@ -7,6 +7,7 @@ https://www.cs.cmu.edu//~rware/assets/pdf/ware-imc2019.pdf
 """
 
 import argparse
+import functools
 import os
 from os import path
 
@@ -17,7 +18,9 @@ import cl_args
 import utils
 
 
-AVG_TPUT_KEY = "average throughput p/s-windowed-minRtt16"
+ARRIVAL_TIME_KEY = "arrival time us"
+TPUT_KEY = "average throughput p/s-windowed-minRtt32"
+# TPUT_KEY = "throughput p/s-ewma-alpha0.003"
 PKT_SIZE_B = 1380
 
 
@@ -26,7 +29,7 @@ def get_avg_tputs(flp):
     with np.load(flp) as fil:
         return (
             utils.Sim(flp),
-            [utils.safe_mean(fil[flw][AVG_TPUT_KEY]) for flw in fil.files])
+            [utils.safe_mean(fil[flw][TPUT_KEY]) for flw in fil.files])
 
 
 def plot_f1b(flps, out_dir):
@@ -38,7 +41,7 @@ def plot_f1b(flps, out_dir):
 
     tot_flws = len(all_ys[0])
     assert tot_flws == 2, \
-        ("This script supports simulations with two flows, but the "
+        ("This figure  supports simulations with two flows, but the "
          f"provided simulation contains {tot_flws} flows!")
 
     # Create the x-values by converting each bottleneck queue size
@@ -59,13 +62,13 @@ def plot_f1b(flps, out_dir):
             # Line with circle markers.
             "o-",
             # The first flow is BBR and the second is Cubic.
-            label=("BBR" if idx == 0 else "Cubic"))
+            label=("1 BBR" if idx == 0 else "1 Cubic"))
 
     plt.xscale("log", basex=2)
     plt.xticks(x_vals, [f"{x:.2f}" if x < 1 else str(round(x)) for x in x_vals])
     plt.xlabel("Queue Size (BDP)")
     plt.ylabel("Throughput (Mbps)")
-    plt.ylim(bottom=0, top=sims[0].bw_Mbps)
+    plt.ylim(bottom=0, top=sims[0].bw_Mbps * 1.1)
     plt.legend()
     plt.tight_layout()
     plt.savefig(path.join(out_dir, "1b.pdf"))
@@ -74,6 +77,44 @@ def plot_f1b(flps, out_dir):
 
 def plot_f1c(flps, out_dir):
     """ Generate figure 1c from Ray's paper. """
+    tot_flps = len(flps)
+    assert tot_flps == 1, \
+        f"This figure uses a single experiment, but {tot_flps} were provided."
+    flp = flps[0]
+    sim = utils.Sim(flp)
+    x_vals = np.arange(300 * 100, dtype=float) / 100
+    with np.load(flp) as fil:
+        tot_flws = len(fil.files)
+        assert tot_flws == 17, \
+            ("This figure  supports simulations with 17 flows, but the "
+             f"provided simulation contains {tot_flws} flows!")
+        tputs = []
+        for flw in range(tot_flws):
+            dat = fil[str(flw)][[ARRIVAL_TIME_KEY, TPUT_KEY]]
+            dat = dat[dat[TPUT_KEY] != -1]
+            # Use the true arrival times and thoughputs to interpolate
+            # the throughput at the same time for each flow.
+            tputs.append(
+                np.interp(
+                    x_vals, xp=dat[ARRIVAL_TIME_KEY] / 1e6,
+                    # Convert from packets/second to Mbps.
+                    fp=dat[TPUT_KEY] * PKT_SIZE_B * 8 / 1e6))
+
+    # The 16 Cubic flows are first. Add up the throughputs of all of
+    # the Cubic flows.
+    unfair_tputs = functools.reduce(np.add, tputs[:-1])
+    fair_tputs = tputs[-1]
+
+    plt.figure(figsize=(8, 3))
+    plt.plot(x_vals, fair_tputs, label="1 BBR")
+    plt.plot(x_vals, unfair_tputs, label="Sum of 16 Cubic")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Throughput (Mbps)")
+    plt.ylim(bottom=0, top=sim.bw_Mbps * 1.1)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(path.join(out_dir, "1c.pdf"))
+    plt.close()
 
 
 def main():
@@ -101,8 +142,8 @@ def main():
     if not path.exists(out_dir):
         os.makedirs(out_dir)
 
-    plot_f1b((path.join(f1b, fln) for fln in os.listdir(f1b)), out_dir)
-    plot_f1c((path.join(f1c, fln) for fln in os.listdir(f1c)), out_dir)
+    plot_f1b([path.join(f1b, fln) for fln in os.listdir(f1b)], out_dir)
+    plot_f1c([path.join(f1c, fln) for fln in os.listdir(f1c)], out_dir)
 
 
 if __name__ == "__main__":
