f517c5d6e36b73f4d988e27e46233c1acbbb618d
Christopher Canel <c.canel@icloud.com>
2022-10-05 23:56:53 +0000

    [WIP] Graph results for all CCAs

--- unfair/scripts/graph_all_ccas.py
+++ unfair/scripts/graph_all_ccas.py
@@ -40,30 +40,30 @@ def main(args):
     results_refactored = {}
     for key, (results_json, results_pickle) in results.items():
         # { (cca1, cca2): (
-        #     0: results_json
-        #     1: jfis_disabled
-        #     2: jfis_enabled
-        #     3: jfis_deltas
-        #     4: jfi_deltas_percent
-        #     5: overall_utils_disabled
-        #     6: overall_utils_enabled
-        #     7: overall_util_deltas_percent
-        #     8: fair_flows_utils_disabled
-        #     9: fair_flows_utils_enabled
-        #     10: fair_flows_util_deltas_percent
-        #     11: unfair_flows_utils_disabled
-        #     12: unfair_flows_utils_enabled
-        #     13: unfair_flows_util_deltas_percent
+        #     0: jfis_disabled
+        #     1: jfis_enabled
+        #     2: jfis_deltas
+        #     3: jfi_deltas_percent
+        #     4: overall_utils_disabled
+        #     5: overall_utils_enabled
+        #     6: overall_util_deltas_percent
+        #     7: fair_flows_utils_disabled
+        #     8: fair_flows_utils_enabled
+        #     9: fair_flows_util_deltas_percent
+        #     10: unfair_flows_utils_disabled
+        #     11: unfair_flows_utils_enabled
+        #     12: unfair_flows_util_deltas_percent
+        #     13: results_pickle
         # )}
-        results_refactored[key] = (results_json, *(list(zip(*results_pickle.values()))))
+        results_refactored[key] = (*(list(zip(*results_json.values()))), results_pickle)
     results = results_refactored
 
     evl.plot_hist(
         args,
         lines=[
             # Extract and flatten the JFIs across all CCAs.
+            [val for vals in results.values() for val in vals[0]],
             [val for vals in results.values() for val in vals[1]],
-            [val for vals in results.values() for val in vals[2]],
         ],
         labels=["Original", "UnfairMon"],
         x_label="JFI",
