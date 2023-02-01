4bc2fd494710703d301ebf72bea782640744af41
Christopher Canel <c.canel@icloud.com>
2023-02-01 13:37:39 -0600

    Bug fixes

--- unfair/scripts/eval.py
+++ unfair/scripts/eval.py
@@ -608,11 +608,11 @@ def main(args):
         target_disabled_name = enabled_exp.name.replace("unfairTrue", "unfairFalse")
         # Strip off trailing timestamp (everything after final "-").
         target_disabled_name = target_disabled_name[
-            : -(reversed(target_disabled_name.name).index("-") + 1)
+            : -(target_disabled_name[::-1].index("-") + 1)
         ]
         target_disabled_exp = None
         for disabled_exp in disabled:
-            if disabled_exp.startswith(target_disabled_name):
+            if disabled_exp.name.startswith(target_disabled_name):
                 target_disabled_exp = disabled_exp
                 break
         if target_disabled_exp is None:
