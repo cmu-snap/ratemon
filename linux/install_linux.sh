3715f3259163f0a4a14ceb03c76fc3274d37f68e
Christopher Canel <c.canel@icloud.com>
2025-09-22 21:26:52 -0700

    Clean up install_linux.sh

--- linux/install_linux.sh
+++ linux/install_linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eoux pipefail
+set -eo pipefail
 
 if [[ $# -ne 1 ]] && [[ $# -ne 2 ]]; then
 	echo "ERROR: Illegal number of parameters."
@@ -12,6 +12,8 @@ version="6.9.12"
 if [[ -n $2 ]]; then
 	version="$2"
 fi
+
+set -ux
 
 sudo apt-get update
 DEBIAN_FRONTEND="noninteractive" sudo apt-get -y --no-install-recommends install \
@@ -34,13 +36,15 @@ tar xf "linux-${version}.tar.gz"
 rm -f "linux-${version}.tar.gz"
 pushd "linux-${version}"
 git apply "${main_dir}/ratemon/linux/get_info_${version}.patch"
-cp -fv "/boot/config-$(uname -r)" .config
+uname="$(uname -r)"
+cp -fv "/boot/config-${uname}" .config
 scripts/config --disable SYSTEM_TRUSTED_KEYS
 scripts/config --disable SYSTEM_REVOCATION_KEYS
-make -j "$(nproc)" menuconfig
-make -j "$(nproc)"
-sudo make -j "$(nproc)" modules_install
-sudo make -j "$(nproc)" install
+nproc="$(nproc)"
+make -j "${nproc}" menuconfig
+make -j "${nproc}"
+sudo make -j "${nproc}" modules_install
+sudo make -j "${nproc}" install
 popd
 
 printf "Please reboot!\n"
