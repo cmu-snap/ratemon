78913ca12384e9425a04df06b64294493907c3a6
Christopher Canel <c.canel@icloud.com>
2026-01-25 23:14:57 -0800

    [WIP] single request mode

--- ratemon/runtime/c/ratemon_tc.bpf.c
+++ ratemon/runtime/c/ratemon_tc.bpf.c
@@ -36,10 +36,13 @@ enum { TC_ACT_OK = 0 };
 inline void handle_extra_grant(struct rm_flow *flow,
                                struct rm_grant_info *grant_info,
                                uint32_t extra_grant, uint32_t *rwnd) {
-  uint32_t rwnd_ = *rwnd;
-  RM_PRINTK("INFO: 'handle_extra_grant' flow %u<->%u granted an extra %u bytes "
-            "on top of desired grant of %u bytes",
-            flow->local_port, flow->remote_port, extra_grant, rwnd_);
+  if (!rwnd) {
+    RM_PRINTK("ERROR: 'handle_extra_grant' flow %u<->%u rwnd ptr is null",
+              flow->local_port, flow->remote_port);
+    return;
+  }
+  RM_PRINTK("INFO: 'handle_extra_grant' flow %u<->%u granted an extra %u bytes",
+            flow->local_port, flow->remote_port, extra_grant);
   // This may go negative (if the extra grant is more than the remaining
   // data). If it does, then this grant will actually pregrant for the
   // sender's next data, which we cannot escape.
